9374afe7920d37a7266ab4f47668b09b651568de
HachemBouhadede <hachembouhadede@gmail.com>
2023-09-28 03:42:59 +0200

    feat(homepage and slider): build a Slider Component, add functionality to it
    creat, build and display the slider for the homepage, with all of it's features
    
    BREAKING CHANGE: we have something to look at in the home page
    
    fix #6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import Image from "next/image"
+import Slider from "@/Components/Slider/Slider"
 
 export default function Home() {
-  return <h1> Hello Amazon Team </h1>
+  return (
+    <>
+      <h1> Hello Amazon Team </h1>
+      <Slider />
+    </>
+  )
 }
