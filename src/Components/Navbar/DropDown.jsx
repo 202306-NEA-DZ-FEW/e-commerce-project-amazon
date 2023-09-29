6b32e58731c7e4322bd26aa6e029f4c39c985b21
hadiadjd <celestialgeek21@gmail.com>
2023-09-30 00:09:11 +0100

    style(navbar): style navbar

--- src/Components/Navbar/DropDown.jsx
+++ src/Components/Navbar/DropDown.jsx
@@ -5,11 +5,12 @@ const DropDown = () => {
   
   return (
     <div className="relative">
-      <button onClick={()=>setOpen(!open)}>click me</button>
+      <button onClick={()=>setOpen(!open)}>Categories</button>
       <div className ={ `${open? "block" : "hidden"} absolute`}>
       <h1>men</h1>
       <h1>women</h1>
       <h1>kids</h1>
+      <h1>electronics</h1>
       </div>
     </div>
   )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,61 +1,73 @@
+import React, { useState, useContext } from "react";
+import { useMediaQuery } from "usehooks-ts";
 import Link from 'next/link';
-import Image from "next/image";
 import AmazonLogo from '../AmazonLogo';
 import DropDown from './DropDown';
-import { BsBag } from 'react-icons/bs'
-import { useState, useContext } from "react";
+import { BsBag } from 'react-icons/bs';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {IoMdClose} from 'react-icons/io';
 import { CartContext} from '@/contexts/CartContext';
 
 
 const Navbar = () => {
   const [searchList, setSearchList] = useState([])
   const [searchInput, setSearchInput] = useState("")
-  const { itemAmount } = useContext(CartContext)
+  const [isopen, setisOpen] = useState(false);
+  const { itemAmount } = useContext(CartContext);
+  const matches = useMediaQuery("(min-width: 768px)");
 
   
   return (
-    <nav className=" bg-white w-full top-0 sticky z-20  flex flex-row items-center justify-between gap-2 mx-2 mb-4">
-      <Link href={"/"}>
-        <AmazonLogo/>
-      </Link>
-      <div className="md:flex hidden">
-        <ul>
-          <li className="inline-block px-4 hover:text-red-700"><Link href={"/"}>men's clothing</Link></li>
-          <li className="inline-block px-4 hover:text-red-700"><Link href={"/"}>women's clothing</Link></li>
-          <li className="inline-block px-4 hover:text-red-700"><Link href={"/"}>jewlery</Link></li>
-          <li className="inline-block px-4 hover:text-red-700 relative"><DropDown/>
-            {/* <ul className="opacity-0 group-hover:opacity-100 absolute bg-blue-500 top-10"><DropDown/></ul> */}
-            </li>
-          <li className="inline-block px-4"><Link href={"/"}>
+    <div>
+  {matches? 
+  <nav className=" bg-white w-full top-0 sticky z-20  flex flex-row items-center justify-between gap-2 mx-2 mb-4">
+  <Link href={"/"}><AmazonLogo/></Link>
+  <div className="flex">
+    <ul className="flex uppercase items-center gap-8">
+      <li className="px-4 hover:text-red-700"><Link href={"/"}>men's clothing</Link></li>
+      <li className="px-4 hover:text-red-700"><Link href={"/"}>women's clothing</Link></li>
+      <li className="px-4 hover:text-red-700"><Link href={"/"}>jewlery</Link></li>
+      <li className="px-4 hover:text-red-700 relative"><DropDown/></li>
+      <li className=" inline-block px-4"><Link href={"/"}>
           <div className='cursor-pointer flex relative'>
-          <BsBag className='text-2xl' />
-          <div className='bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items center'>
-            {itemAmount}
-          </div>
-        </div>
-            
-            </Link></li>
-
-        </ul>
-      </div>
-      <div className=" md:flex hidden flex-row  w-fit items-center gap-4">
-        <div className="flex items-center mt-2">
-            <div className="flex border border-red-200 rounded">
-                <input
-                    type="text"
-                    className="block px-1 py-2 text-red-700 bg-white border rounded-md focus:border-red-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                    placeholder="Search..."
-                    value={searchInput}
-            onChange={(e) => handleSeacrh(e)}
-                />
-                <button className="px-4 text-white bg-red-600 border-l rounded ">
-                    Search
-                </button>
+            <BsBag className='text-2xl' />
+            <div className='bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items center'>
+        {itemAmount}
             </div>
+          </div>
+        </Link>
+      </li>
+    </ul>
+  </div>
+  
+</nav> : 
+<nav className="h-[80%]">
+  <div className="text-xl z-10" onClick={() => setisOpen(!isopen)}>
+    {isopen ? (<IoMdClose/>) : (<GiHamburgerMenu/>)}
+    <div className="bg-white fixed w-full ">
+    <ul className={`bg-bkg fixed  w-64 z-20 top-0 overflow-y-auto bottom-0 py-24 pl-4 duration-500 left-0 ${isopen ? "right-0" : "right-[-100%]"}`}>
+      <li className="inline-block px-4 hover:text-red-700"><Link href={"/"}>men's clothing</Link></li>
+      <li className="inline-block px-4 hover:text-red-700"><Link href={"/"}>women's clothing</Link></li>
+      <li className="inline-block px-4 hover:text-red-700"><Link href={"/"}>jewlery</Link></li>
+      <li className="inline-block px-4 hover:text-red-700 relative"><DropDown/>
+        {/* <ul className="opacity-0 group-hover:opacity-100 absolute bg-blue-500 top-10"><DropDown/></ul> */}
+        </li>
+      <li className=" inline-block px-4">
+        <Link href={"/"}>
+          <div className='cursor-pointer flex relative'>
+            <BsBag className='text-2xl' />
+              <div className='bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items center'>
+        {itemAmount}
         </div>
-      </div>
-    </nav>
-  )
-}
-
+    </div>            
+        </Link>
+      </li>
+      </ul>
+  </div>
+  </div>
+  
+      
+</nav>}
+</div>
+)}
 export default Navbar;
