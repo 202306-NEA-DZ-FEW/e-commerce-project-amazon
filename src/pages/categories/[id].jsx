3fb08fda3180e058ba5dc4b0ee62ba4e86e759b6
HachemBouhadede <hachembouhadede@gmail.com>
2023-09-30 01:42:05 +0200

    feat(categories page): fetch data and links for the page
    use getstaticprops and getstaticpath in fetch data nd links, currently displaying data as json
    
    fix 18

---
+++ src/pages/categories/[id].jsx
@@ -0,0 +1,36 @@
+import { GET_PRODUCTS_BY_CATEGORY, GET_ALL_CATEGORIES } from "@/lib/constants";
+import { fetchApi } from "@/lib/utils/fetchApi";
+
+export default function Category({ products }) {
+    JSON.stringify(products)
+  return (
+    <div>
+        
+    </div>
+  );
+}
+
+export async function getStaticProps({ params }) {
+  const products = await fetchApi(GET_PRODUCTS_BY_CATEGORY + params.id);
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60,
+  };
+}
+
+export async function getStaticPaths() {
+  const categories = await fetchApi(GET_ALL_CATEGORIES);
+
+  // Generate an array of objects with 'params' property for dynamic paths
+  const paths = categories.map((category) => ({
+    params: { id: category }, // Assuming 'category' is a string
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,40 @@
 import Head from "next/head"
 import Slider from "@/Components/HomepageComponents/Slider"
 import HomePageProduct from "@/Components/HomepageComponents/HomePL"
+import { fetchApi } from "@/lib/utils/fetchApi"
+import { GET_ALL_CATEGORIES } from "@/lib/constants"
+import { useEffect } from "react"
 
-export default function Home({ products }) {
+export default function Home({ products, categories}) {
+
+useEffect = (() => {
+  setCategories (categories)
+}, [])
+
   return (
     <>
       <Head>
         <title>AmaZD by Amazon Team</title>
       </Head>
       <h1> Hello Amazon Team </h1>
-      <Slider />
+      <Slider categories={categories}/>
       <HomePageProduct products={products} />
     </>
   )
 }
 
 export async function getStaticProps() {
-  const res = await fetch("https://fakestoreapi.com/products?limit=15")
-  const products = await res.json()
-  console.log(products)
+  const resP = await fetch("https://fakestoreapi.com/products?limit=15")
+  const products = await resP.json()
+  const categories =  await fetchApi(GET_ALL_CATEGORIES)
+  // console.log(products)
   return {
     props: {
       products,
+      categories
     },
     revalidate: 60,
+
   }
 }
+
