5934abad7dc4829f0f9f98efb320125c68a9f843
HachemBouhadede <hachembouhadede@gmail.com>
2023-09-30 03:11:48 +0200

    feat(categories pages, routing, consts): display the fetched data, add cover picture, add routing
    use the data fetched by getstaticprops to display a list of catagery items, implement a logic to get
    a dynamic  cover picture, add the consts file (and i'm happy to say 0 chatGPT was used here)
    
    BREAKING CHANGE: we have a categories page !
    
    fix 18

--- src/pages/categories/[id].jsx
+++ src/pages/categories/[id].jsx
@@ -1,9 +1,42 @@
-import { GET_PRODUCTS_BY_CATEGORY, GET_ALL_CATEGORIES } from "@/lib/constants"
+import {
+  GET_PRODUCTS_BY_CATEGORY,
+  GET_ALL_CATEGORIES,
+  slides
+} from "@/lib/constants"
 import { fetchApi } from "@/lib/utils/fetchApi"
+import ProductCard from "@/Components/ProductCard/ProductICard"
 
 export default function Category({ products }) {
-  JSON.stringify(products)
-  return <div></div>
+
+
+  const slideCat = slides.find((slide) => slides.category === products.category);
+
+  return (
+    <div>
+      <div className="max-w-[1400px] h-[780px] w-full m-auto py-4 px-4 relative group">
+        <div
+          style={{ backgroundImage: `url("${slideCat?.url || ""}")` }}
+          className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+        >
+          <div className="relative top-[35%] inset-0 space-y-5 flex flex-col items-center justify-center">
+            {/* Category name */}
+            <div className="flex items-center justify-center  bg-black/40 border-y-4 w-full">
+              <h2 className="text-3xl font-bold text-white p-4 ">
+                {products.category}
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="grid grid-cols-1 mt-4 mx-4 gap-4 md:grid-cols-3 lg:grid-cols-4">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export async function getStaticProps({ params }) {
@@ -22,7 +55,7 @@ export async function getStaticPaths() {
 
   // Generate an array of objects with 'params' property for dynamic paths
   const paths = categories.map((category) => ({
-    params: { id: category }, // Assuming 'category' is a string
+    params: { id: category }, // 
   }))
 
   return {
