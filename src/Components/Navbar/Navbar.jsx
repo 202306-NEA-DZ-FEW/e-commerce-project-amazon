fa259be22a43990548826a1ecf4460c7df163459
ismailbenlaredj <yako_is@yahoo.fr>
2023-09-30 12:12:59 +0100

    style(responsive nav): navbar styling

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-"use client";
+// "use client";
 import { useState, useContext } from "react"
-import { useMediaQuery } from "usehooks-ts"
+// import { useMediaQuery } from "usehooks-ts"
 import Link from "next/link"
 import AmazonLogo from "../AmazonLogo"
 import DropDown from "./DropDown"
@@ -8,87 +8,136 @@ import { BsBag } from "react-icons/bs"
 import { GiHamburgerMenu } from "react-icons/gi"
 import { IoMdClose } from "react-icons/io"
 import { CartContext } from "@/contexts/CartContext"
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai"
 
 const Navbar = () => {
   const [searchList, setSearchList] = useState([])
   const [searchInput, setSearchInput] = useState("")
-  const [isopen, setisOpen] = useState(false)
+  const [isOpen, setisOpen] = useState(false)
   const { itemAmount } = useContext(CartContext)
-  const matches = useMediaQuery("(min-width: 768px)")
+  // const matches = useMediaQuery("(min-width: 768px)")
 
   return (
-    <div>
-      {matches ? (
-        <nav className=" bg-white w-full top-0 sticky z-20  flex flex-row items-center justify-between gap-2 mx-2 mb-4">
-          <Link href={"/"}>
-            <AmazonLogo />
-          </Link>
-          <div className="flex">
-            <ul className="flex uppercase items-center gap-8">
-              <li className="px-4 hover:text-red-700">
-                <Link href={"/"}>men's clothing</Link>
-              </li>
-              <li className="px-4 hover:text-red-700">
-                <Link href={"/"}>women's clothing</Link>
-              </li>
-              <li className="px-4 hover:text-red-700">
-                <Link href={"/"}>jewlery</Link>
-              </li>
-              <li className="px-4 hover:text-red-700 relative">
-                <DropDown />
-              </li>
-              <li className=" inline-block px-4">
-                <Link href={"/"}>
-                  <div className="cursor-pointer flex relative">
-                    <BsBag className="text-2xl" />
-                    <div className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items center">
-                      {itemAmount}
-                    </div>
-                  </div>
-                </Link>
-              </li>
-            </ul>
+    <nav className=" relative">
+      <div className="flex flex-col lg:flex-row h-full items-center justify-between p-4">
+        <div className="flex flex-row w-full justify-between items-center">
+          <div className="w-fit">
+            <Link href="/">
+              <AmazonLogo />
+            </Link>
           </div>
-        </nav>
-      ) : (
-        <nav className="h-[80%]">
-          <div className="text-xl z-100" onClick={() => setisOpen(!isopen)}>
-            {isopen ? <IoMdClose /> : <GiHamburgerMenu />}
-            <div className="bg-white w-full ">
-              <ul
-                className={`bg-bkg fixed  w-64 z-20 top-0 overflow-y-auto bottom-0 py-24 pl-4 duration-500 left-0 ${
-                  isopen ? "right-0" : "right-[-100%]"
-                }`}
-              >
-                <li className="inline-block px-4 hover:text-red-700">
-                  <Link href={"/"}>men's clothing</Link>
-                </li>
-                <li className="inline-block px-4 hover:text-red-700">
-                  <Link href={"/"}>women's clothing</Link>
-                </li>
-                <li className="inline-block px-4 hover:text-red-700">
-                  <Link href={"/"}>jewlery</Link>
-                </li>
-                <li className="inline-block px-4 hover:text-red-700 relative">
-                  <DropDown />
-                  {/* <ul className="opacity-0 group-hover:opacity-100 absolute bg-blue-500 top-10"><DropDown/></ul> */}
-                </li>
-                <li className=" inline-block px-4">
-                  <Link href={"/"}>
-                    <div className="cursor-pointer flex relative">
-                      <BsBag className="text-2xl" />
-                      <div className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items center">
-                        {itemAmount}
-                      </div>
-                    </div>
-                  </Link>
-                </li>
-              </ul>
+          {isOpen ? (
+            <div
+              className=" w-10 cursor-pointer lg:hidden"
+              onClick={() => {
+                setisOpen(false)
+              }}
+            >
+              <AiOutlineClose className="text-2xl" />
+            </div>
+          ) : (
+            <div
+              className={`w-10 lg:hidden cursor-pointer`}
+              onClick={() => setisOpen(true)}
+            >
+              <AiOutlineMenu className="text-2xl mt-1 mr-1" />
+            </div>
+          )}
+        </div>
+        <div
+          className={`${
+            isOpen ? "flex" : "hidden"
+          } mt-5  relative lg:flex flex-col space-y-10 lg:space-y-0 w-full h-full items-center justify-around lg:justify-end lg:flex-row`}
+        >
+          <div className="flex flex-col space-y-2 md:space-y-0 lg:flex-row items-center">
+            <div className="text-l lg:mx-7 lg:text-link font-medium">
+              <a href="#services"> men clothing</a>
+            </div>
+            <div className="text-l lg:mx-7 lg:text-link font-medium">
+              <a href="#me">women clothing</a>
+            </div>
+            <div className="text-l lg:mx-7 lg:text-link font-medium">
+              <a href="/blog"> jewlery</a>
             </div>
           </div>
-        </nav>
-      )}
-    </div>
+        </div>
+      </div>
+    </nav>
   )
+
+  // return (
+  //   <div>
+  //     {matches ? (
+  //       <nav className=" bg-white w-full top-0 sticky z-20  flex flex-row items-center justify-between gap-2 mx-2 mb-4">
+  //         <Link href={"/"}>
+  //           <AmazonLogo />
+  //         </Link>
+  //         <div className="flex">
+  //           <ul className="flex uppercase items-center gap-8">
+  //             <li className="px-4 hover:text-red-700">
+  //               <Link href={"/"}>men's clothing</Link>
+  //             </li>
+  //             <li className="px-4 hover:text-red-700">
+  //               <Link href={"/"}>women's clothing</Link>
+  //             </li>
+  //             <li className="px-4 hover:text-red-700">
+  //               <Link href={"/"}>jewlery</Link>
+  //             </li>
+  //             <li className="px-4 hover:text-red-700 relative">
+  //               <DropDown />
+  //             </li>
+  //             <li className=" inline-block px-4">
+  //               <Link href={"/"}>
+  //                 <div className="cursor-pointer flex relative">
+  //                   <BsBag className="text-2xl" />
+  //                   <div className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items center">
+  //                     {itemAmount}
+  //                   </div>
+  //                 </div>
+  //               </Link>
+  //             </li>
+  //           </ul>
+  //         </div>
+  //       </nav>
+  //     ) : (
+  //       <nav className="h-[80%]">
+  //         <div className="text-xl z-100" onClick={() => setisOpen(!isopen)}>
+  //           {isopen ? <IoMdClose /> : <GiHamburgerMenu />}
+  //           <div className="bg-white w-full ">
+  //             <ul
+  //               className={`bg-bkg fixed  w-64 z-20 top-0 overflow-y-auto bottom-0 py-24 pl-4 duration-500 left-0 ${
+  //                 isopen ? "right-0" : "right-[-100%]"
+  //               }`}
+  //             >
+  //               <li className="inline-block px-4 hover:text-red-700">
+  //                 <Link href={"/"}>men's clothing</Link>
+  //               </li>
+  //               <li className="inline-block px-4 hover:text-red-700">
+  //                 <Link href={"/"}>women's clothing</Link>
+  //               </li>
+  //               <li className="inline-block px-4 hover:text-red-700">
+  //                 <Link href={"/"}>jewlery</Link>
+  //               </li>
+  //               <li className="inline-block px-4 hover:text-red-700 relative">
+  //                 <DropDown />
+  //                 {/* <ul className="opacity-0 group-hover:opacity-100 absolute bg-blue-500 top-10"><DropDown/></ul> */}
+  //               </li>
+  //               <li className=" inline-block px-4">
+  //                 <Link href={"/"}>
+  //                   <div className="cursor-pointer flex relative">
+  //                     <BsBag className="text-2xl" />
+  //                     <div className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] h-[18px] text-white rounded-full flex justify-center items center">
+  //                       {itemAmount}
+  //                     </div>
+  //                   </div>
+  //                 </Link>
+  //               </li>
+  //             </ul>
+  //           </div>
+  //         </div>
+  //       </nav>
+  //     )}
+  //   </div>
+  // )
 }
 export default Navbar
