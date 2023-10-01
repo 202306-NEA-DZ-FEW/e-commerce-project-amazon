28d7ac6bc4a61f773f40a3e874b1dfc0530134f3
ismailbenlaredj <yako_is@yahoo.fr>
2023-10-01 17:02:55 +0100

    feat(footer): add a footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,79 +1,76 @@
-import Item from "./Item";
 import { AiFillGithub, AiOutlineLinkedin } from "react-icons/ai";
 import Image from "next/image";
-import Men from "../../assets/Men.svg";
+// import Men from "../../assets/Men.svg";
 
-const members=[
+const members = [
   {
-    id:1,
-    name:"HACHEM BOUHADENE",
-    github:"https://github.com/HachemBouhadede",
-    linkedin:"https://www.linkedin.com/in/hachem-bouhadede/",
+    id: 1,
+    name: "HACHEM BOUHADENE",
+    github: "https://github.com/HachemBouhadede",
+    linkedin: "https://www.linkedin.com/in/hachem-bouhadede/",
+    src: "/assets/team/hachem.jpeg",
   },
   {
-    id:2,
-    name:"ISMAIL BENLAREDJ",
-    github:"https://github.com/ismail-benlaredj",
-    linkedin:"https://www.linkedin.com/in/ismailbenlaredj/",
+    id: 2,
+    name: "ISMAIL BENLAREDJ",
+    github: "https://github.com/ismail-benlaredj",
+    linkedin: "https://www.linkedin.com/in/ismailbenlaredj/",
+    src: "/assets/team/ismail.jpeg",
   },
   {
-    id:3,
-    name:"HAKIM TIABI",
-    github:"https://github.com/Hxkim10",
-    linkedin:"https://www.linkedin.com/in/abdelhakimtiabi/",
+    id: 3,
+    name: "HAKIM TIABI",
+    github: "https://github.com/Hxkim10",
+    linkedin: "https://www.linkedin.com/in/abdelhakimtiabi/",
+    src: "/assets/team/hakim.jpg",
   },
   {
-    id:4,
-    name:"HADIA DJADALLAH",
-    github:"https://github.com/liliumorion",
-    linkedin:"https://www.linkedin.com/in/hadia-djadallah/",
+    id: 4,
+    name: "HADIA DJADALLAH",
+    github: "https://github.com/liliumorion",
+    linkedin: "https://www.linkedin.com/in/hadia-djadallah/",
+    src: "/assets/team/hadia.jpeg",
   },
   {
-    id:5,
-    name:"MERIEM MANSOURI",
-    github:"https://github.com/meryem08",
-    linkedin:"https://github.com/meryem08",
+    id: 5,
+    name: "MERIEM MANSOURI",
+    github: "https://github.com/meryem08",
+    linkedin: "https://github.com/meryem08",
+    src: "/assets/team/hadia.jpeg",
   },
   {
-    id:6,
-    name:"DALILA MUEGENNI" ,
+    id: 6,
+    name: "DALILA MUEGENNI",
     github: "https://github.com/dalilia",
-    linkedin:"https://www.linkedin.com",
+    linkedin: "https://www.linkedin.com",
+    src: "/assets/team/hadia.jpeg",
   },
 ]
 const Footer = () => {
   return (
-    <footer className="bg-darkgray text-whitebeige mt-24 w-full">
-      <div className=" text-center grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-6 gap-4 sm:px-8 px-6 pt-16 pb-8 ">
-        <div className="drop-shadow-md hover:shadow-md bg-white p-4 rounded-lg transition duration-300 transform hover:-translate-y-2">
-        <div className="object-contain">
-        <ul className="flex-col items-center ">
-          {members.map((member) => (<>
-          <Image
-          src={Men}
-          width={400}
-          height={400}
-          alt="Illustration"
-          className="w-full rounded-lg bg-cover transform scale-100 filter  transition-all duration-300 ease-in-out hover:filter-blur-0"/>  
-          
-            <li key={member.name} className=" text-base flex-row items-center  align-middle">
-               <h3 className="text-sm mb-1">{member.name}</h3>
-              <a
-                className="text-whitebeige hover:text-red-700 duration-300 text-sm cursor-pointer leading-6 "
-                href={member.link}
-                target="_blank"
-                rel="noopener"
-              >
-              {member.github}
-              {member.linkedin}
-              </a>
-              
-            </li>
-            </>
-  ))}
-    </ul>
-    </div>
-    </div>
+    <footer className="bg-darkgray  mt-24">
+      <div className="  ">
+        <ul className="flex-col flex items-center lg:flex-row justify-between w-full">
+          {members.map((member) => (
+            <div className="mt-24  flex flex-col items-center  " key={member.id}>
+              <Image
+                src={member.src}
+                width={100}
+                height={100}
+                alt="Illustration"
+                className="w-40 lg:w-20  rounded-lg bg-cover transform filter  transition-all duration-300 ease-in-out hover:filter-blur-0" />
+
+              <li key={member.name} className=" flex-row items-center justify-center  ">
+
+                <h3 className=" text-1xl mb-1 text-center my-5 font-semibold">{member.name}</h3>
+                <div className="flex justify-center gap-4 mt-5">
+                  <a href={member.github} target="_blank"><AiFillGithub className="text-3xl cursor-pointer" /></a>
+                  <a href={member.linkedin} target="_blank"><AiOutlineLinkedin className="text-3xl cursor-pointer" /></a>
+                </div>
+              </li>
+            </div>
+          ))}
+        </ul>
       </div>
       <div
         className="w-full p-4 text-center"
@@ -84,7 +81,7 @@ const Footer = () => {
 };
 
 export default Footer;
- 
+
 
 
 
@@ -103,37 +100,37 @@ export default Footer;
       name: 'Hachem',
       linkedin: 'https://www.linkedin.com/in/user1/',
       github: 'https://github.com/user1',
-      illustration: '/card1.jpg', // Provide a path to your illustration
+      illustration: '/card1.jpeg', // Provide a path to your illustration
     },
     {
       name: 'Hachem',
       linkedin: 'https://www.linkedin.com/in/user1/',
       github: 'https://github.com/user1',
-      illustration: '/card1.jpg', // Provide a path to your illustration
+      illustration: '/card1.jpeg', // Provide a path to your illustration
     },
     {
       name: 'Hachem',
       linkedin: 'https://www.linkedin.com/in/user1/',
       github: 'https://github.com/user1',
-      illustration: '/card1.jpg', // Provide a path to your illustration
+      illustration: '/card1.jpeg', // Provide a path to your illustration
     },
     {
       name: 'Hachem',
       linkedin: 'https://www.linkedin.com/in/user1/',
       github: 'https://github.com/user1',
-      illustration: '/card1.jpg', // Provide a path to your illustration
+      illustration: '/card1.jpeg', // Provide a path to your illustration
     },
     {
       name: 'Hachem',
       linkedin: 'https://www.linkedin.com/in/user1/',
       github: 'https://github.com/user1',
-      illustration: '/card1.jpg', // Provide a path to your illustration
+      illustration: '/card1.jpeg', // Provide a path to your illustration
     },
     {
       name: 'Hachem',
       linkedin: 'https://www.linkedin.com/in/user1/',
       github: 'https://github.com/user1',
-      illustration: '/card1.jpg', // Provide a path to your illustration
+      illustration: '/card1.jpeg', // Provide a path to your illustration
     },
   
     // Add more cards in a similar format
