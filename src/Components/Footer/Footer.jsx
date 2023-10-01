ec1176f91af4a3d1fa80d980745a52d7a4bb03be
hadiadjd <celestialgeek21@gmail.com>
2023-10-01 15:01:48 +0100

    style(footer): add styling

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,13 +3,7 @@ import { HACHEM, ISMAIL, HAKIM, HADIA, MERIEM, DALILA } from "./members";
 const Footer = () => {
   return (
     <footer className="bg-darkgray text-whitebeige mt-24 w-full">
-      <div className="md:flex md:justify-center md:items-center sm:px-12 px-4 bg-bkg/20 py-4">
-        <h1
-          className="lg:text-xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5 text-center justify-center"
-        >
-        </h1>
-      </div>
-      <div className=" text-center grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-6 gap-4 sm:px-8 px-6 py-16">
+      <div className=" text-center grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-6 gap-4 sm:px-8 px-6 pt-16 pb-8 ">
       <Item Links={HACHEM} title="HACHEM BOUHADENE" />
       <Item Links={ISMAIL} title="ISMAIL BENLAREDJ" />
       <Item Links={HAKIM} title="HAKIM TIABI" />
@@ -29,3 +23,72 @@ const Footer = () => {
 };
 
 export default Footer;
+ 
+/*const Footer = () => {
+  const cards = [
+    {
+      name: 'Hachem',
+      linkedin: 'https://www.linkedin.com/in/user1/',
+      github: 'https://github.com/user1',
+      illustration: '/card1.jpg', // Provide a path to your illustration
+    },
+    {
+      name: 'Hachem',
+      linkedin: 'https://www.linkedin.com/in/user1/',
+      github: 'https://github.com/user1',
+      illustration: '/card1.jpg', // Provide a path to your illustration
+    },
+    {
+      name: 'Hachem',
+      linkedin: 'https://www.linkedin.com/in/user1/',
+      github: 'https://github.com/user1',
+      illustration: '/card1.jpg', // Provide a path to your illustration
+    },
+    {
+      name: 'Hachem',
+      linkedin: 'https://www.linkedin.com/in/user1/',
+      github: 'https://github.com/user1',
+      illustration: '/card1.jpg', // Provide a path to your illustration
+    },
+    {
+      name: 'Hachem',
+      linkedin: 'https://www.linkedin.com/in/user1/',
+      github: 'https://github.com/user1',
+      illustration: '/card1.jpg', // Provide a path to your illustration
+    },
+    {
+      name: 'Hachem',
+      linkedin: 'https://www.linkedin.com/in/user1/',
+      github: 'https://github.com/user1',
+      illustration: '/card1.jpg', // Provide a path to your illustration
+    },
+  
+    // Add more cards in a similar format
+  ];
+
+  return (
+    <footer className="bg-white text-grey-700 py-12">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-8">
+        {cards.map((card, index) => (
+          <div key={index} className="bg-gray-700 p-4 rounded-lg transition duration-300 transform hover:-translate-y-2">
+            <img src={card.illustration} alt={card.name} className=" mb-4" />
+            <h2 className="text-xl font-bold mb-2">{card.name}</h2>
+            <div className="flex items-center mb-2">
+              <a href={card.linkedin} target="_blank" rel="noopener noreferrer" className="mr-2">
+                LinkedIn
+              </a>
+              <a href={card.github} target="_blank" rel="noopener noreferrer">
+                GitHub
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+ */
+
+
--- src/Components/Footer/Item.jsx
+++ src/Components/Footer/Item.jsx
@@ -1,26 +1,35 @@
-import {AiFillGithub, AiOutlineLinkedin} from "react-icons/ai";
+import Image from "next/image";
+import Men from "../../assets/Men.svg"
 const Item = ({ Links, title }) => {
-  return (<>
-    <ul>
-      <h3 className="mb-1">{title}</h3>
+  return (<div className="drop-shadow-md hover:shadow-md bg-white p-4 rounded-lg transition duration-300 transform hover:-translate-y-2">
+    <div className="object-contain">
+                <Image
+                  src={Men}
+                  width={400}
+                  height={400}
+                  alt="Illustration"
+                  className="w-full rounded-lg bg-cover transform scale-100 filter  transition-all duration-300 ease-in-out hover:filter-blur-0"
+                />
+              </div>
+    <ul className="flex-col items-center ">
+      <h3 className="text-sm mb-1">{title}</h3>
       {Links &&
-        Links.map((link) => (
-          <li key={link.name}>
-            <a
-              className="text-whitebeige hover:text-red-700 duration-300 text-sm cursor-pointer leading-6 "
-              href={link.link}
-              target="_blank"
-              rel="noopener"
-            >
-              {link.name}
-            </a>
-          </li>
-        ))}
+          Links.map((link) => (
+            <li key={link.name} className=" text-base flex-row items-center  align-middle">
+              <a
+                className="text-whitebeige hover:text-red-700 duration-300 text-sm cursor-pointer leading-6 "
+                href={link.link}
+                target="_blank"
+                rel="noopener"
+              >
+                {link.icon}
+              </a>
+              
+            </li>
+          ))}
     </ul>
-   {/*  <div className="object-contain">
-add image ?
-          </div> */}
-    </>
+   
+    </div>
   );
 };
 // i  want to add an icon fr each link in the map through menus.js, but idk if it works in deployment.
