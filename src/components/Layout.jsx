6cc72933b7fdd26e924795bb16b12434661430de
Ismail Benlaredj <55237234+ismail-benlaredj@users.noreply.github.com>
2023-10-02 11:25:13 +0100

    update src

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Components/Navbar/Navbar";
+import { fetchApi } from "@/lib/utils/fetchApi";
+import { GET_ALL_CATEGORIES } from "@/lib/constants";
+import Footer from "./footer/Footer";
+export default function Layout({ children }) {
+  return (
+    <div className="w-fit-screen">
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
+}
---
+++ src/pages/products/[productId].jsx
@@ -0,0 +1,45 @@
+import Head from "next/head";
+import dynamic from "next/dynamic";
+import ProductCardDetailsPlaceHolder from "@/components/singleProductComponents/productCardDetails/ProductCardDetailsPlaceHolder";
+import ProductImagePlaceHolder from "@/components/singleProductComponents/productImage/ProductImagePlaceHolder";
+import { getProductById } from "@/lib/utils/fetchApi";
+
+const ProductImage = dynamic(
+  () => import("@/components/singleProductComponents/productImage"),
+  {
+    loading: () => <ProductImagePlaceHolder />,
+  },
+);
+const ProductCardDetails = dynamic(
+  () => import("@/components/singleProductComponents/ProductCardDetails"),
+  {
+    loading: () => <ProductCardDetailsPlaceHolder />,
+  },
+);
+
+function ProductDetails({ product }) {
+  return (
+    <>
+      <Head>
+        <title>{product.title}</title>
+        <meta name="description" content="Product details" />
+      </Head>
+      <main className="grid gap-8 md:grid-cols-6 xl:px-20 p-8  ">
+        <ProductImage product={product} />
+        <ProductCardDetails product={product} />
+      </main>
+    </>
+  );
+}
+
+export default ProductDetails;
+
+export async function getServerSideProps(context) {
+  const { productId } = context.query;
+  const product = await getProductById(productId);
+  return {
+    props: {
+      product,
+    },
+  };
+}
